9175bafaf6c800ffcd669950fed434da02337a29
Beomgu <jb98967@gmail.com>
2024-04-06 14:26:54 -0400

    Add tokenizing module v1

---
+++ v1/tokenizing/main4.c
@@ -0,0 +1,8 @@
+#include "tokenizing.h"
+
+int main(void)
+{
+	// Call the tokenizing function defined in the tokenizing module
+	tokenizing();
+	return 0;
+}
---
+++ v1/tokenizing/tokenizing.h
@@ -0,0 +1,8 @@
+// Tokenizing module header
+#ifndef _TOKENIZING_H_
+#define _TOKENIZING_H_
+#include <stdio.h>
+#include <string.h>
+// declare function prototype
+void tokenizing(void);
+#endif

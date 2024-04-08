d5cff930d20a41935347dbdc08ab18c6cb61a36a
Beomgu <jb98967@gmail.com>
2024-04-07 21:02:51 -0400

    Add converting module v1

---
+++ v1/converting/main3.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+// 
+//
+//  Created by Parisa jalali on 2024-04-07.
+//
+#include "converting.h"
+
+int main(void)
+{
+    // Call the converting function defined in the converting module
+    converting();
+    return 0;
+}

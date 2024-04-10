87bc4e5e7555b971cac001133d943fa5c6a8a593
Beomgu <jb98967@gmail.com>
2024-04-10 13:50:01 -0400

    Add manipulating module v1

---
+++ v1/manipulating/manipulating.h
@@ -0,0 +1,13 @@
+// MANIPULATING MODULE HEADER  
+
+// Header guard to prevent multiple inclusion of the same header file.
+#ifndef _MANIPULATING_H_
+#define _MANIPULATING_H_
+// Include standard input/output library for printf function.
+#include <stdio.h>
+// Include string manipulation library for string-related functions.
+#include <string.h>
+// Declaration of the manipulating function prototype.
+void manipulating (void);
+// End of the header guard.
+#endif
